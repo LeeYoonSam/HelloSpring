b8fa822f09b4220e9d0f7af63b7b04a36eb6e714
YS <yoonsam86@gmail.com>
2020-08-22 22:20:26 +0900

    Section 3. 회원 도메인과 리파지토리 만들기

---
+++ src/main/java/com/ys/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.ys.hellospring.repository;
+
+import com.ys.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
