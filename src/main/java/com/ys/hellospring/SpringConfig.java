43652a63f54713bb6ef2a9b482f7b865ca458aca
YS <yoonsam86@gmail.com>
2020-08-26 21:08:32 +0900

    Section 7. 순수 JDBC

--- src/main/java/com/ys/hellospring/SpringConfig.java
+++ src/main/java/com/ys/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package com.ys.hellospring;
 
+import com.ys.hellospring.repository.JdbcMemberRepository;
 import com.ys.hellospring.repository.MemberRepository;
-import com.ys.hellospring.repository.MemoryMemberRepository;
 import com.ys.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
